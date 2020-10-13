cdd551307185e1fc295508ea1590ea3077df0f63
LJ <69187341+I-am-just-LJ@users.noreply.github.com>
2020-10-13 21:43:26 +0900

    update file

--- code-challenge5/index.js
+++ code-challenge5/index.js
@@ -1,20 +1,32 @@
-const addTask = document.querySelector(".addTask"),
-  inputTask = addTask.querySelector("input"),
+const addTasks = document.querySelector(".addTask"),
+  inputTask = addTasks.querySelector("input"),
   pendingList = document.querySelector(".pendingList");
 
 const TASK_LS = "Task";
-const Task = [];
+let Task = [];
 
-function saveToTask() {
+function saveTask() {
   localStorage.setItem(TASK_LS, JSON.stringify(Task));
+}
+
+function deleteTask(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  pendingList.removeChild(li);
+  const cleanTask = Task.filter(function (Tasks) {
+    return Tasks.id !== parseInt(li.id);
+  });
+  Task = cleanTask;
+  saveTask();
 }
 
 function addTask(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteTask);
   const span = document.createElement("span");
-  const newId = Date.now();
+  const newId = "inputTask.value";
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(delBtn);
@@ -25,7 +37,7 @@ function addTask(text) {
     id: newId,
   };
   Task.push(TaskObj);
-  saveToTask();
+  saveTask();
 }
 
 function handleSubmit(event) {
@@ -39,13 +51,15 @@ function loadTask() {
   const loadedTask = localStorage.getItem(TASK_LS);
   if (loadedTask !== null) {
     const parsedTask = JSON.parse(loadedTask);
-    parsedTask.forEach(function(Task));
+    parsedTask.forEach(function (Tasks) {
+      addTask(Tasks.text);
+    });
   }
 }
 
 function init() {
   loadTask();
-  addTask.addEventListener("submit", handleSubmit);
+  addTasks.addEventListener("submit", handleSubmit);
 }
 
 init();
